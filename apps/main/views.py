10470909783b60f29bb006effedefe0c9255c877
Andrew W. Burger <andrew.burger@awbtech.org>
2019-02-19 00:34:40 -0500

    Finished homepage

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
    return render(request, 'main/index.html')
+
+def shop(request):
+   return render(request, 'main/shop-category.html')
+
+def services(request):
+   return render(request, 'main/services.html')
